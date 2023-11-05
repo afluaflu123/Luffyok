108cc62f46523dbd2eea5f2768200c0c95b0b0ce
HacKer Jr ~ TG <83758692+afluaflu123@users.noreply.github.com>
2023-11-05 14:21:51 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -43,7 +43,9 @@ BUTTONS0 = {}
 BUTTONS1 = {}
 BUTTONS2 = {}
 SPELL_CHECK = {}
-# ENABLE_SHORTLINK = ""
+CLICK = {}
+
+max_clicks = 1
 
 @Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client, message):
@@ -776,27 +778,30 @@ async def cb_handler(client: Client, query: CallbackQuery):
             alert = alerts[int(i)]
             alert = alert.replace("\\n", "\n").replace("\\t", "\t")
             await query.answer(alert, show_alert=True)
-        
+
     if query.data.startswith("file"):
+        ident, file_id = query.data.split("#")
         clicked = query.from_user.id
         try:
             typed = query.message.reply_to_message.from_user.id
         except:
-            typed = query.from_user.id
-        ident, file_id = query.data.split("#")
+            typed = clicked
+        if typed != clicked:
+            return await query.answer(script.ALRT_TXT.format(query.from_user.first_name), show_alert=True)
         files_ = await get_file_details(file_id)
         if not files_:
             return await query.answer('Nᴏ sᴜᴄʜ ғɪʟᴇ ᴇxɪsᴛ.')
         files = files_[0]
         title = files.file_name
         size = get_size(files.file_size)
-        f_caption = files.caption
+        f_caption = files.caption    
         settings = await get_settings(query.message.chat.id)
+        button_data = f"{query.message.id}-{query.data}"
         if CUSTOM_FILE_CAPTION:
             try:
                 f_caption = CUSTOM_FILE_CAPTION.format(file_name='' if title is None else title,
                                                        file_size='' if size is None else size,
-                                                       file_caption='' if f_caption is None else f_caption)
+                                                       file_caption=title if f_caption is None else f_caption)
             except Exception as e:
                 logger.exception(e)
             f_caption = f_caption
@@ -804,51 +809,51 @@ async def cb_handler(client: Client, query: CallbackQuery):
             f_caption = f"{files.file_name}"
 
         try:
-            if (AUTH_CHANNEL or REQ_CHANNEL) and not await is_subscribed(client, query):
+            if AUTH_CHANNEL and not await is_subscribed(client, query):
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
                 return
             elif settings['botpm']:
                 await query.answer(url=f"https://t.me/{temp.U_NAME}?start={ident}_{file_id}")
-                await query.answer('Cʜᴇᴄᴋ Pᴍ, I Hᴀᴠᴇ Sᴇɴᴛ Fɪʟᴇ Iɴ Pᴍ\n\n©️ @Team_KL', show_alert=True)
                 return
             else:
-                file_send=await client.send_cached_media(
-                    chat_id=FILE_CHANNEL,
+                if button_data in CLICK and CLICK[button_data] >= max_clicks:
+                    await query.answer("ϐяο ϲℓιϲκ οи τнє ϐοττοм яιgнτ мєиτιοи ϐυττοи 💕\n\nതാഴെ വലതുഭാഗത് ഒരു മെൻഷൻ ലിങ്ക് ഉണ്ട് അതിൽ ക്ലിക്ക് ചെയ്യുക 💕", show_alert=True)
+                    return
+                mh = await client.send_cached_media(
+                    chat_id=int(FILE_CHANNEL),
                     file_id=file_id,
-                    caption=script.CHANNEL_CAP.format(query.from_user.mention, title, query.message.chat.title),
+                    caption=script.FILE_MSG.format(query.from_user.mention, f_caption, size),
                     protect_content=True if ident == "filep" else False,
                     reply_markup=InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton("🔥 ᴄʜᴀɴɴᴇʟ 🔥", url=(MAIN_CHANNEL))
-                            ]
-                        ]
+                        [[ 
+                          InlineKeyboardButton('⚠️ ᴍᴀʟ', callback_data='malayalam'),
+                          InlineKeyboardButton('⚠️ ʜɪɴ', callback_data='hindi'),
+                          InlineKeyboardButton('⚠️ ᴛᴀᴍ', callback_data='tamil')
+                        ],[
+                          InlineKeyboardButton("мαιи ϲнαииєℓ", url='t.me/CxMaxTG')
+                        ]] 
                     )
                 )
-                Joel_tgx = await query.message.reply_text(
-                    script.FILE_MSG.format(query.from_user.mention, title, size),
-                    parse_mode=enums.ParseMode.HTML,
-                    reply_markup=InlineKeyboardMarkup(
+                mh8 = await query.message.reply(script.CHANNEL_CAP.format(query.from_user.mention, afsu, title, size, uptime),
+                True,
+                enums.ParseMode.HTML,
+                disable_web_page_preview=True,
+                reply_markup=InlineKeyboardMarkup(
+                    [
                         [
-                         [
-                          InlineKeyboardButton('📥 Rᴇᴏ̨ᴜᴇsᴛ Rᴇᴅɪʀᴇᴄᴛ Cʜᴀɴɴᴇʟ 📥 ', url = (FILE_FORWARD))
-                       ],[
-                          InlineKeyboardButton("⚠️ Nᴏᴡ Cʟɪᴄᴋ Hᴇʀᴇ Fᴏʀ Fɪʟᴇ 🥰 ⚠️", url=file_send.link)
-                         ]
+                            InlineKeyboardButton("📥 ∂οωиℓοα∂ иοω 📥", url=f"{mh.link}")
                         ]
-                    )
+                    ]
                 )
-                if settings['auto_delete']:
-                    await asyncio.sleep(100)
-                    await Joel_tgx.delete()
-                    await file_send.delete()
-        except UserIsBlocked:
-            await query.answer('Uɴʙʟᴏᴄᴋ ᴛʜᴇ ʙᴏᴛ ᴍᴀʜɴ !', show_alert=True)
-        except PeerIdInvalid:
-            await query.answer(url=f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}")
+            )
+            CLICK[button_data] = CLICK.get(button_data, 0) + 1
+            await asyncio.sleep(240)
+            await mh8.delete()
+            await mh.delete()
+            del mh8, mh
         except Exception as e:
-            await query.answer(url=f"https://telegram.me/{temp.U_NAME}?start={ident}_{file_id}")
-            
+            logger.exception(e, exc_info=True)
+        
     elif query.data.startswith("sendfiles"):
         clicked = query.from_user.id
         ident, key = query.data.split("#")
